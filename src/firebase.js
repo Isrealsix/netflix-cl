c6e94b963d878df034ddfb0296c5a5b74139d8db
Isreal6ix <isrealsix@gmail.com>
2021-08-19 00:09:03 +0700

    vagitus

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyCBQ560y0HONjrjBlrDlg7d8NMhTcwv2e4',
+	authDomain: 'netflix-484f0.firebaseapp.com',
+	projectId: 'netflix-484f0',
+	storageBucket: 'netflix-484f0.appspot.com',
+	messagingSenderId: '418711113361',
+	appId: '1:418711113361:web:73b7d8e3ec20f1982ef930',
+};
