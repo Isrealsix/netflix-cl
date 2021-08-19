31306de1fc8ddd284e543473947f8d716c3e3e39
Isreal6ix <isrealsix@gmail.com>
2021-08-19 23:15:07 +0700

    completed the function to register and sign in

--- src/components/pages/signUpWindow/SignUpWindow.js
+++ src/components/pages/signUpWindow/SignUpWindow.js
@@ -1,19 +1,43 @@
+import { useRef } from 'react';
+import { auth } from '../../../firebase';
 import './SignUpWindow.scss';
 
 const SignUpWindow = () => {
+	const passwordRef = useRef('');
+	const emailRef = useRef('');
 	const handleRegistration = ev => {
 		ev.preventDefault();
+		auth
+			.createUserWithEmailAndPassword(
+				emailRef.current.value,
+				passwordRef.current.value
+			)
+			.then(authUser => {
+				console.log(authUser);
+			})
+			.catch(error => {
+				alert(error);
+			});
 	};
 
 	const handleSignin = ev => {
 		ev.preventDefault();
+		auth
+			.signInWithEmailAndPassword(
+				emailRef.current.value,
+				passwordRef.current.value
+			)
+			.then(authUser => {})
+			.catch(error => {
+				alert(error);
+			});
 	};
 	return (
 		<div className="signUpWindow">
 			<form>
 				<h1>Sign In</h1>
-				<input type="email" placeholder="Enter Email" />
-				<input type="password" placeholder="Enter Password" />
+				<input type="email" ref={emailRef} placeholder="Enter Email" />
+				<input type="password" ref={passwordRef} placeholder="Enter Password" />
 				<button type="submit" onClick={handleSignin}>
 					Sign In
 				</button>
