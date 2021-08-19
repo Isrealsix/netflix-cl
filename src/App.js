7cdbacbe4c5db07b4af79cfe35173bca015a4be7
Isreal6ix <isrealsix@gmail.com>
2021-08-19 23:22:01 +0700

    added the functionality to get logged in user, persist their section and used the cleanup function to clear last onAuthStateChanged

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import HomeScreen from './components/pages/home/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/pages/login/Login';
+import { auth } from './firebase';
 
 function App() {
 	const user = null;
+
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged(userAuth => {
+			if (userAuth) {
+				// still logged In
+				console.log(userAuth);
+			} else {
+				// Not Logged In
+			}
+		});
+
+		return unsubscribe;
+	}, []);
 	return (
 		<div className="app">
 			<Router>
