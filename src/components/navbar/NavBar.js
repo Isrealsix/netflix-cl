0668c7fa646c40505b73dfb6b07a60c978010519
Isreal6ix <isrealsix@gmail.com>
2021-08-20 13:58:49 +0700

    fixed the broken image

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -27,7 +27,7 @@ const NavBar = () => {
 			<div className="navbar__content">
 				<img
 					className="navbar__logo"
-					src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
 					alt=""
 					onClick={() => history.push('/')}
 				/>
